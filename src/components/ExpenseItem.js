dcd2a4d0806b4720898556307576bf0beb80a566
Bouhouch Marouane <bouhouch.marouane@gmail.com>
2022-05-03 01:51:23 +0000

    Styling ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>ExpemseItem</h2>;
+    return (
+      <div class="card m-3">
+        <div class="card-header">Car insurance</div>
+        <div class="card-body">
+          <h5 class="card-title">4000 dh</h5>
+          <p class="card-text">09/01/2022</p>
+        </div>
+      </div>
+    );
 }
 
 export default ExpenseItem;
