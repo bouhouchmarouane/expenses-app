72b11173664590f54ac8a2ef721c31d49bde99ab
Bouhouch Marouane <bouhouch.marouane@gmail.com>
2022-06-18 01:45:04 +0100

    adding message for empty list

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,20 +1,30 @@
 import ExpenseItem from "./ExpenseItem";
 
 const Expenses = (props) => {
-  return (
-    <div>
-      {props.expenseItems.filter(expenseItem => expenseItem.date.getFullYear() == props.filterYearSelected || props.filterYearSelected == '')
-      .map(expenseItem => (
-        <ExpenseItem
+  const filteredExpenses = props.expenseItems.filter(
+    (expenseItem) =>
+      expenseItem.date.getFullYear() == props.filterYearSelected ||
+      props.filterYearSelected === ""
+  );
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expenseItem) => (
+      <ExpenseItem
         key={expenseItem.id}
         title={expenseItem.title}
         amount={expenseItem.amount}
         date={expenseItem.date}
         paymentMethod={expenseItem.paymentMethod}
       />
-      ))}
+    ));
+  }
+  return (
+    <div>
+      {expensesContent}
     </div>
   );
-}
+};
 
 export default Expenses;
